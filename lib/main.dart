f090cbec686ac92c558a84db395911b10bd3ff81
ruben.ruiz <rubenruizpedreira@gmail.com>
2023-04-10 01:25:51 +0200

    :recycle: refactor home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:yaru/yaru.dart';
 
-import 'components/note_card.dart';
 import 'pages/create_note.dart';
-import 'utils/notes_storage.dart';
+import 'pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +18,7 @@ class MyApp extends StatelessWidget {
       home: Builder(
         builder: (context) => YaruTheme(
           data: AppTheme.of(context),
-          child: const MyHomePage(title: "IdeaGenius"),
+          child: const HomePage(),
         ),
       ),
       debugShowCheckedModeBanner: false,
@@ -57,72 +56,6 @@ class AppTheme {
         themeMode: themeMode,
         variant: variant,
         highContrast: highContrast,
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Note> _notes = [];
-
-  Future<void> _visitCreateNotePage() async {
-    final note = await Navigator.pushNamed(context, "/create-note") as Note?;
-    if (note != null) {
-      await NotesStorage.addNote(note);
-      _loadNotes();
-    }
-  }
-
-  Future<void> _loadNotes() async {
-    final notes = await NotesStorage.getNotes();
-    setState(() {
-      _notes = notes;
-    });
-  }
-
-  Future<void> _deleteNote(Note note) async {
-    await NotesStorage.deleteNote(note);
-    _loadNotes();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _loadNotes();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: _notes.length,
-        itemBuilder: (context, index) {
-          final note = _notes[index];
-          return NoteCard(
-            color: Color(int.parse(note.color)),
-            title: note.title,
-            content: note.content,
-            date: note.date,
-            onDelete: () => _deleteNote(note),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _visitCreateNotePage,
-        tooltip: 'Add',
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16.0),
-        ),
-        child: const Icon(Icons.add),
       ),
     );
   }
